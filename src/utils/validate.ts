45f5e2fa1981dcc8ec9a7748333fe9e1848839e2
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-15 20:08:50 -0400

    Updated validate function

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,48 +1,59 @@
 import validator from "validator";
 
-const inferType = (name: string) => {
-  const includesKeyword = (keyword: string) => name.toLowerCase().includes(keyword);
-  if (includesKeyword("email")) return "email";
-  if (includesKeyword("password")) return "password";
-  return "default";
+const name = (value: string) => {
+  const isEmpty = !value;
+
+  if (isEmpty) return "Name is required";
+  return "";
 };
 
-const validate = {
-  all: (inputs: {[x: string]: string}) => {
-    const errors = {};
+const email = (value: string) => {
+  const isEmpty = !value;
+  const isInvalid = !validator.isEmail(value);
 
-    for (const i in inputs) {
-      if (inputs[i] !== undefined) {
-        const type = inferType(i);
-        errors[i] = validate[type](inputs[i]);
-      }
-    }
-    return errors;
-  },
-
-  input: (name: string, value: string) => {
-    const type = inferType(name);
-    return validate[type](value);
-  },
-
-  email: (email: string) => {
-    if (!email) return " ";
-    if (!validator.isEmail(email)) return "Email is invalid";
-    return "";
-  },
-
-  password: (password: string) => {
-    if (!password) return " ";
-    const checkCase = password !== password.toLowerCase();
-    const checkSize = password.length > 7;
-
-    if (!checkCase && !checkSize) return "Password must have 8 characters and one capital letter";
-    if (!checkCase) return "Password must have a capital letter";
-    if (!checkSize) return "Password must have 8 characters";
-    return "";
-  },
-
-  default: (i: string) => !i,
+  if (isEmpty) return "Email is required";
+  if (isInvalid) return "Email is invalid";
+  return "";
 };
 
-module.exports = validate;
+const password = (value: string) => {
+  const isEmpty = !value;
+  const isTooShort = value.length < 8;
+  const isMissingUpperCase = value === value.toLowerCase();
+
+  if (isEmpty) return "Password is required";
+  if (isTooShort) return "Password must have at least 8 characters";
+  if (isMissingUpperCase) return "Password must have a capital letter";
+  return "";
+};
+
+const url = (value: string) => {
+  const isEmpty = !value;
+  const isInvalid = !validator.isURL(value); // does not check for http:// or https://
+  const isNotSecure = value.substring(0, 8) !== "https://";
+
+  if (isEmpty) return "URL is required";
+  if (isInvalid) return "URL is invalid";
+  if (isNotSecure) return "URL must begin with https://";
+  return "";
+};
+
+const date = (value: string) => {
+  const isEmpty = !value;
+  const isInvalid = !validator.isDate(value, { format: "YYYY-MM-DD", delimiters: ["-"] });
+
+  if (isEmpty) return "Date is required";
+  if (isInvalid) return "Date is invalid";
+  return "";
+};
+
+const time = (value: string) => {
+  const isEmpty = !value;
+  const isInvalid = !/(?:[01][0-9]|2[0-3]):(?:[0-5][0-9])/.test(value);
+
+  if (isEmpty) return "Time is required";
+  if (isInvalid) return "Time is invalid";
+  return "";
+};
+
+export default { email, password, url, date, time };
